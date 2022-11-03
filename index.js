03c44dd875c73befad63e595a768eec1c01d0017
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2022-11-03 21:41:26 +0100

    Updated CRUD methods for Mongoose

--- index.js
+++ index.js
@@ -3,10 +3,151 @@ const express = require(('express'));
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
 
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/electricCinema', { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
 
 //app.use(morgan('common'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//CREATE 
+
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+//READ
+//Get all users
+
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch ((err) => {
+      console.error(err);
+      res.status(500).send('Error bro ' + err);
+    });
+ });
+
+ //Get all movies
+
+ app.get('/movies', (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch ((err) => {
+      console.error(err);
+      res.status(500).send('Error bro ' + err);
+    });
+ });
+
+
+
+
+//GET one specific user by username
+
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+//UPDATE
+
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+//DELETE
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.')
+  })
 
 /*let topMovies = [
     {
@@ -58,7 +199,7 @@ app.use(bodyParser.json());
     });
     
     app.use(express.static('public')); */
-
+/*
     let users = [
       {
         id: 1,
@@ -253,6 +394,7 @@ app.use(express.static('public'));
  res.status(500).send('Something broke!');
     }); */
     
-app.listen(8080, () => {
+/* app.listen(8080, () => {
 console.log('Your app is listening on port 8080.')
 })
+*/
